623f4898eb4abd9ccae146d401f293d60a6d0389
aryaputrabalinda13 <aryaputrabalinda13@students.amikom.ac.id>
2026-07-31 04:51:57 +0700

    ujicoba

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin Dashboard - AmikomEventHub</title>
+    <title>Admin Dashboard - AmikomEventHubHubHubHub</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@300;400;500;600;700&display=swap"
         rel="stylesheet">
